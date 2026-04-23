39a816480e79cf1e2f6954d54046e8d4e0004203
Matt Griffin <mifga@users.noreply.github.com>
2026-04-23 07:05:02 -0400

    fixed backtick error in serve.js
    This error was already found by dan and fixed, not sure why it doesn't get updated here. I need to make sure I'm pulling the right updates.

--- web/mygame/mygame.js
+++ web/mygame/mygame.js
@@ -1,3 +1,22 @@
-nav = new SceneNavigator(["startup"]);
-stats = {};
+﻿nav = new SceneNavigator([
+ "startup",
+ "animal",
+ "variables",
+ "gosub",
+ "ending",
+ "death"
+]);
+stats = {
+ "choice_title": "My First ChoiceScript Game",
+ "leadership": "50",
+ "strength": "50"
+};
+purchases = {};
+achievements = [];
+nav.setStartingStatsClone(stats);if (achievements.length) {
+  nav.loadAchievements(achievements);
+}
+if (nav.loadProducts) nav.loadProducts([], purchases);
+
+isCogPublished = true;
 
